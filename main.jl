5cafc169b49c211723ea90f5007be4e38610cab8
Ulrich Baron-Fournier <ulrichbaronfournier@gmail.com>
2023-10-06 23:29:17 -0400

    main graph fonction

--- main.jl
+++ main.jl
@@ -6,7 +6,6 @@ include("graph.jl")
 function convert_to_edge(edge, weight)
     node1 = Node(str(edge(1)), edge(1))
     node2 = Node(str(edge(2)), edge(2))
-
     edge_c = Edge(string(edge(1)) * "-" * string(edge(2)), node1, node2, weight)
     return edge_c
 end
@@ -15,8 +14,8 @@ function graph_from_instance(filename::String)
     header = read_header(filename)
     nodes = read_nodes(header, filename)
     edges, weights = read_edges(header, filename)
-    
-
-    
+    edges_c = convert_to_edge.(edges,weights)
+    graph = Graph("graph_name", nodes, edges_c)
+    return graph
  end
  
