2a352e14b32ebd9dd5058a46ea321d2e5d960354
Ulrich Baron-Fournier <ulrichbaronfournier@gmail.com>
2023-10-09 14:24:00 -0400

    add pluto_phase1

--- pluto_phase1.jl
+++ pluto_phase1.jl
@@ -1,4 +1,56 @@
+### A Pluto.jl notebook ###
+# v0.19.27
+
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ d60f02ce-0a83-4edb-9747-b3de9ba1ae2b
+using Markdown
+
+# ╔═╡ 5dfaa4bb-f544-43df-a390-a9e326784fed
+using InteractiveUtils
+
+# ╔═╡ 7ea81498-700d-4402-bea4-37b5203d088f
 using Logging
+
+# ╔═╡ 5b0505f0-ab35-4ebc-9458-1914a54c7bfa
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+InteractiveUtils = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+Logging = "56ddb016-857b-54e1-b83d-db4d58db5568"
+Markdown = "d6f4376e-aef5-505a-96c1-9c027394607a"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.9.3"
+manifest_format = "2.0"
+project_hash = "348ed7e828d2091a44e211d4df367eb5f2d0eb19"
+
+[[deps.Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+
+[[deps.InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[deps.Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+
+[[deps.Markdown]]
+deps = ["Base64"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+"""
+
+# ╔═╡ Cell order:
+# ╠═d60f02ce-0a83-4edb-9747-b3de9ba1ae2b
+# ╠═5dfaa4bb-f544-43df-a390-a9e326784fed
+# ╠═7ea81498-700d-4402-bea4-37b5203d088f
+# ╠═5b0505f0-ab35-4ebc-9458-1914a54c7bfa
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
